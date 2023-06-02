ee9169c3e2638109dbd4ec57a3d4413c9458973f
holly <h.person.notrobot@gmail.com>
2023-06-02 23:17:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let number = 5e+14
+current_min = 0
+current_max = (5e+14)*2
+let number_box = document.getElementById("number")
+
+number_box.innerText = "000000000000000"
+
+function greater(){
+    current_min = number;
+    number = parseInt((current_max + current_min) / 2)
+    number_box.innerText = number.toString()
+}
+function lesser(){
+    current_max = number;
+    number = parseInt((current_max + current_min) / 2)
+    number_box.innerText = number.toString()
+}
